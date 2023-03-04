f9c0607426ac6e043b03bf8da64d40a778c09a64
RecursionExcursion <rloup15@gmail.com>
2023-03-03 19:19:41 -0500

    Completed API class, map to obj to SerObs, Save to local memory, and ablity to serialize and deserialize

---
+++ CFB_Ranker/Persistence/PersistenceManager.cs
@@ -0,0 +1,25 @@
+﻿
+
+using CFB_Ranker.Persistence.Serializable_Models;
+using CFB_Ranker.Persistence.Serialization;
+using Newtonsoft.Json;
+
+namespace CFB_Ranker.Persistence
+{
+    public class PersistenceManager
+    {
+        private readonly string dir = @"C:\Users\rloup\Main Folder\Programming\Workspaces\VS-workspace\CFB_Ranker\" +
+                        @"CFB_Ranker\Persistence\Team_Data\";
+        private readonly string filename = "file.json";
+
+        public Season LoadData()
+        {
+            if (!File.Exists(dir + filename))
+            {
+                Season season = new SeasonMapper().BuildSeason();
+                JSONSerializer.WriteJsonToFile<Season>(dir + filename, season);
+            }
+            return JSONSerializer.ReadJsonFromFile<Season>(dir + filename)!;
+        }
+    }
+}
--- CFB_Ranker/Program.cs
+++ CFB_Ranker/Program.cs
@@ -6,19 +6,8 @@ using CFB_Ranker.Persistence.Serializable_Models;
 using CFB_Ranker.Persistence.Serialization;
 Console.WriteLine("Hello, World!");
 
-string dir = @"C:\Users\rloup\Main Folder\Programming\Workspaces\VS-workspace\CFB_Ranker\CFB_Ranker\Persistence\Team_Data\";
-string filename = "file.json";
 
-Season? season = null;
 
-if (File.Exists(dir + filename))
-{
-    season = SerializationManager.ReadJsonFromFile<Season>(dir + filename);
-} else
-{
-    SerializationManager.LoadDateFromAPI();
-    SerializationManager.WriteJsonToFile<Season>(dir + filename);
-}
 
 
 Console.WriteLine();
